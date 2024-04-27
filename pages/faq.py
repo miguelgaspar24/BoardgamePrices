d2cfe35a04cbdee98db2835a350025cb68eef9da
Miguel Gaspar <miguelgaspar24@gmail.com>
2024-04-27 22:03:30 +0100

    Implement multipages, by creating a new, standard landing page and moving our previous main page content to the "analytics" page

--- pages/faq.py
+++ pages/faq.py
@@ -1,20 +1,12 @@
 
-#import dash
+import dash
 from dash import html
 
 
-#dash.register_page(__name__)
+dash.register_page(__name__, path='/faq')
 
 layout = html.Div(
-        children=[
-            html.Div(
                 children=[
-                    html.H1(children='The Price is Right: Board Game Edition', className='header-title'),
-                    html.P(children=('This is a test page for the "use_pages" functionality of Dash'),
-                           className='header-description'),
-                    html.Div(className='header-title')
-                        ],
-                className='header'
+                    html.P(children=('This is a test page for the "use_pages" functionality of Dash'))
+                        ]
             )
-        ]
-)
---
+++ pages/not_found_404.py
@@ -0,0 +1,8 @@
+
+import dash
+from dash import html
+
+
+dash.register_page(__name__)
+
+layout = html.H3("This is the landing page")
