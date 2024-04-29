e8c5a8549e05801c85fa0d2fa960ced68ed44ec8
Miguel Gaspar <miguelgaspar24@gmail.com>
2024-04-29 23:33:39 +0100

    Create default message for undefined pages, including the landing age, to avoid the default 404 error

--- pages/not_found_404.py
+++ pages/not_found_404.py
@@ -5,4 +5,5 @@ from dash import html
 
 dash.register_page(__name__)
 
-layout = html.H3("This is the landing page")
+layout = html.Div(children=[html.H4('Welcome to the landing page.', className='page-text'),
+                            html.H4('Please select a functional page from the navigation bar at the top.', className='page-text')])
