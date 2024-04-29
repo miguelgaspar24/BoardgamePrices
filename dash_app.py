b46e38c9804d05e4a91c1f71acd4735c8dfb86af
Miguel Gaspar <miguelgaspar24@gmail.com>
2024-04-29 23:30:55 +0100

    Update dash_app to be a landing page required for a multi-page app, and moved the core scraping and analytics from here to pages/main.py

--- dash_app.py
+++ dash_app.py
@@ -7,31 +7,30 @@ import dash_bootstrap_components as dbc
 external_stylesheets=[dbc.themes.BOOTSTRAP, dbc.icons.BOOTSTRAP]
 
 app = Dash(__name__,
+           title='The Price is Right',
            pages_folder='pages',
            use_pages=True,
            external_stylesheets=external_stylesheets)
-
-app.title = 'The Price is Right'
 
 app.layout = html.Div(
                 children=[
                     dbc.NavbarSimple(
                                 children=[
-                                    dbc.NavItem(dbc.NavLink('Analytics', href='/analytics')),
-                                    dbc.NavItem(dbc.NavLink('FAQ', href='/faq'))
+                                    dbc.NavItem(dbc.NavLink('Main', href='/main')),
+                                    dbc.NavItem(dbc.NavLink('FAQ', href='/faq')),
+                                    dbc.NavItem(dbc.NavLink('About', href='/about'))
                                 ],
                                 id='navbar',
-                                brand='NavbarSimple',
+                                brand='Home',
                                 brand_href='/',
                                 color='#222222',
-                                dark=True,
+                                dark=True
                             ),
                     html.Div(
                         children=[
                             html.H1(children='The Price is Right: Board Game Edition', className='header-title'),
                             html.P(children='🃏♟️🎲🧩', className='header-emoji'),
-                            html.P(children=('Track the prices of board games across online vendors to find the best deals at any given time'),
-                                className='header-description')
+                            html.P(children=('Track the prices of board games across online vendors to find the best deals at any given time!'), className='header-description')
                                 ],
                         className='header'
                     ),
